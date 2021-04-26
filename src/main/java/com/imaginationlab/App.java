019de6eec224f04e037d402f7bc96a0bf543e41e
chilam <chilam.Lee@outlook.com>
2021-04-26 22:19:37 +0800

    :rocket: feat: 完成基本的框架

--- src/main/java/com/imaginationlab/App.java
+++ src/main/java/com/imaginationlab/App.java
@@ -15,24 +15,26 @@ public class App extends Application {
 
     private static Scene scene;
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(App.class.getResource("com.imaginationlab.view." + fxml +
+                        ".fxml"));
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("launchPanel"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
